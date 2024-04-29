1c9289c15473705c06758cd814ec89e4f367c93d
alekhyaku <a1.kuchimanchi@gmail.com>
2024-04-28 21:11:36 -0500

    final final

--- rl/plots/plot.py
+++ rl/plots/plot.py
@@ -1,17 +1,17 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from rl.testing.test_all import seeds, maps
+from rl.testing.test_all import maps
 
 # List of algorithms, maps, and seeds
 algorithms = ['ddpg', 'dqn', 'ppo', 'ppo_discrete', 'baseline']
-
+seeds = [253, 557]
 # Load the data from the CSV files
 dataframes = []
 for algorithm in algorithms:
     for map_name in maps:
         for seed in seeds:
-            df = pd.read_csv(f'{algorithm}_{map_name}_seed{seed}_return.csv', header=None, names=['Episode', 'Reward'])
+            df = pd.read_csv(f'{algorithm}_{map_name}_{seed}_return.csv', header=None, names=['Episode', 'Reward'])
             df['Algorithm'] = algorithm
             df['Map'] = map_name
             df['Seed'] = seed
